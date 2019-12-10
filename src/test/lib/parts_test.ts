lit/lit

Reparent nodes into detached DocumentFragment when clearing NodePart
Imagine this template structure:

```js
render(html`${
  // Outer NodePart
  ${
    html`${
      // Nested NodePart here
      directive()
    }`
  }
}`, container)
```

When the `NodePart` renders a `TemplateResult`, we get a structure somewhat like:

```html
<!--NodePart's Start-->
<!--    nested NodePart's Start-->
<!--    nested NodePart's End-->
<!--NodePart's End-->
```

This is because we take the `TemplateResult`'s template fragment (which has the nested `NodePart`'s start/end nodes as direct children), and inject it directly between the outer `NodePart`'s start/end nodes.

When we change the value that's rendered into that part, we would clear out everything between the `NodePart`'s. That leaves the `TemplateResult`'s nested `NodePart`'s start/end nodes completely detached (no `parentNode`). When the nested `NodePart` tries to update (via an async directive), we would try to mutate the nested `nodePart.startNode.parentNode` (which is `null`)!

This fixes it in a hacky way be reparenting those cleared nodes into a `DocumentFragment`. The nested `NodePart` will have now be attached to the `DocumentFragment`, meaning it can update without issue.

Fixes #293

src/test/lib/parts_test.ts
@@ -12,7 +12,7 @@
  * http://polymer.github.io/PATENTS.txt
  */
 
-import {AttributeCommitter, AttributePart, createMarker, DefaultTemplateProcessor, EventPart, html, NodePart, render, templateFactory, TemplateResult} from '../../lit-html.js';
+import {AttributeCommitter, AttributePart, createMarker, DefaultTemplateProcessor, EventPart, html, NodePart, render, templateFactory, TemplateResult, directive, Part} from '../../lit-html.js';
 import {stripExpressionMarkers} from '../test-utils/strip-markers.js';
 
 const assert = chai.assert;
@@ -515,6 +515,46 @@ suite('Parts', () => {
         part.clear();
         assert.deepEqual(
             Array.from(container.childNodes), [startNode, endNode]);
+      });
+
+      test('clears nodes into a detached fragment', () => {
+        const text = document.createTextNode('foo');
+        container.insertBefore(text, endNode);
+        part.clear();
+        assert.notEqual(text.parentNode, null);
+        assert.equal(text.parentNode!.nodeType, Node.DOCUMENT_FRAGMENT_NODE);
+      });
+    });
+
+    suite('directive holds NodePart', () => {
+      test('does not throw errors when part is detached', () => {
+        let part: Part;
+        const fooDirective = directive(() => (p: Part) => {
+          part = p;
+        });
+
+        const t = (bool: boolean) => html`<div>${
+          bool
+          ? html`${fooDirective()}`
+          : 'detached'
+        }</div>`;
+
+        // First render with unresolved Promise
+        render(t(true), container);
+        assert.equal(stripExpressionMarkers(container.innerHTML), '<div></div>');
+
+        // Simulate an async part settting.
+        part!.setValue('foo');
+        part!.commit();
+
+        // Now detach the part's wrapping TemplateResult
+        render(t(false), container);
+        assert.equal(stripExpressionMarkers(container.innerHTML), '<div>detached</div>');
+
+        // Simulate an async part settting.
+        part!.setValue('bar');
+        part!.commit();
+        assert.equal(stripExpressionMarkers(container.innerHTML), '<div>detached</div>');
       });
     });
   });

IliaIdakiev/lit-html

src/test/lib/parts_test.ts
@@ -13,7 +13,7 @@
  */
 
 import {__testOnlyClearSanitizerFactoryDoNotCallOrElse, setSanitizerFactory} from '../../lib/parts.js';
-import {AttributeCommitter, AttributePart, createMarker, DefaultTemplateProcessor, EventPart, html, NodePart, render, templateFactory, TemplateResult} from '../../lit-html.js';
+import {AttributeCommitter, AttributePart, createMarker, DefaultTemplateProcessor, directive, EventPart, html, NodePart, Part, render, templateFactory, TemplateResult} from '../../lit-html.js';
 import {stripExpressionMarkers} from '../test-utils/strip-markers.js';
 
 const assert = chai.assert;
@@ -517,6 +517,46 @@ suite('Parts', () => {
         part.clear();
         assert.deepEqual(
             Array.from(container.childNodes), [startNode, endNode]);
+      });
+
+      test('clears nodes into a detached fragment', () => {
+        const text = document.createTextNode('foo');
+        container.insertBefore(text, endNode);
+        part.clear();
+        assert.notEqual(text.parentNode, null);
+        assert.equal(text.parentNode!.nodeType, Node.DOCUMENT_FRAGMENT_NODE);
+      });
+    });
+
+    suite('directive holds NodePart', () => {
+      test('does not throw errors when part is detached', () => {
+        let part: Part;
+        const fooDirective = directive(() => (p: Part) => {
+          part = p;
+        });
+
+        const t = (bool: boolean) =>
+            html`<div>${bool ? html`${fooDirective()}` : 'detached'}</div>`;
+
+        // First render with unresolved Promise
+        render(t(true), container);
+        assert.equal(
+            stripExpressionMarkers(container.innerHTML), '<div></div>');
+
+        // Simulate an async part settting.
+        part!.setValue('foo');
+        part!.commit();
+
+        // Now detach the part's wrapping TemplateResult
+        render(t(false), container);
+        assert.equal(
+            stripExpressionMarkers(container.innerHTML), '<div>detached</div>');
+
+        // Simulate an async part settting.
+        part!.setValue('bar');
+        part!.commit();
+        assert.equal(
+            stripExpressionMarkers(container.innerHTML), '<div>detached</div>');
       });
     });
   });